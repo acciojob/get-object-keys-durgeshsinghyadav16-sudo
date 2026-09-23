c1d78927c588a345a6786ec29d6bf36823142136
durgeshsinghyadav16-sudo <durgeshsinghyadav16@gmail.com>
2026-09-23 22:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+// Create the student object
 const student = {
   name: "John",
 };
 
-
+// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
 // Testing the function
-console.log(getKeys(student));
+console.log(getKeys(student)); // Output: ["name"]
